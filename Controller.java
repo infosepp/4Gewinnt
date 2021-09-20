ea04672df9b87357408bfbf76f77f622182c0847
infosepp <71786937+infosepp@users.noreply.github.com>
2021-09-20 16:37:45 +0200

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -22,9 +22,24 @@ public class Controller
 
     }
 
+    /**
+     * Diese Methode ermöglicht es zu Spielen (Spielschleife). Über die Buttons der Anzeige wird
+     * die Spalte ermittlet in die der Spieler den Stein werfen möchte.
+     */
     public void spielen()
     {
-
+        while(modell.pruefeGewonnen() == false)
+        {
+            for(int i = 0; i < anzeige.getButtons().length; i++)
+            {
+                if(anzeige.getButtons()[i].clicked())
+                {
+                    modell.setzeStein(i);
+                    updateView();
+                }
+            }
+            anzeige.getView().wait(10);
+        }
     }
     
     private void updateView()
