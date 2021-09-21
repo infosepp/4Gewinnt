eb33d7d5b985897b20add096b3ec4b8adb0553c9
annahuelshoff <91113929+annahuelshoff@users.noreply.github.com>
2021-09-21 08:59:20 +0200

    Update Modell.java

--- Modell.java
+++ Modell.java
@@ -74,11 +74,19 @@ public class Modell
         
     }
 
+    /**
+     * Wenn vier Steine einer Farbe in einer Diagonalen von links nach rechts vorhanden sind, gebe true zurück, ansonsten false.
+     * @return Wahrheitswert
+     */
     private boolean pruefeVierDiagonalLR ()
     {
         
     }
 
+    /**
+     * Wenn vier Steine einer Farbe in einer Diagonalen von rechts nach links vorhanden sind, gebe true zurück, ansonsten false.
+     * @return Wahrheitswert
+     */
     private boolean pruefeVierDiagonalRL ()
     {
         
