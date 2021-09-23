07a8e8e0ba9710268d9b0cbf9c10af6cb03213d3
Raphaél Karagün <raphael.karaguen@mga.bistum365.de>
2021-09-23 08:06:12 +0200

    test

--- Spieler.java
+++ Spieler.java
@@ -21,24 +21,52 @@ public class Spieler
     {
 
     }
+<<<<<<< HEAD
 
     /**
      * 
+=======
+    
+    /**
+     * Weist dem Array steine neue Steinobjekte zu.
+>>>>>>> bc55c6027ff18d76a50606db6bb62f62cb2efcdd
      */
     public void initSteine()
     {
 
     }
+<<<<<<< HEAD
 
     /**
      * 
+=======
+    
+    /**
+     * Gibt zurück, ob in dem Array steine noch Steinobjekte vorhanden sind.
+     * 
+     * @return Wahrheitswert
+>>>>>>> bc55c6027ff18d76a50606db6bb62f62cb2efcdd
      */
     public boolean steineVorhanden()
     {
+<<<<<<< HEAD
 
+=======
+        
+    }
+    
+   /**
+    * Gibt die Frabe der Objekte zurück.
+    * @return Color 
+    */ 
+    public Color getColor()
+    {
+        
+>>>>>>> bc55c6027ff18d76a50606db6bb62f62cb2efcdd
     }
     
     /**
+<<<<<<< HEAD
      *Gibt die Frabe der Objekte zurück.
      * @return Color 
      */ 
@@ -49,15 +77,32 @@ public class Spieler
 
     /**
      * 
+=======
+     * Gibt zurück, ob der aktuelle Stein das Steinobjekt an Position 17 im Array steine ist,
+     * da damit der Spieler auch der Startspieler ist.
+     * 
+     * @return Wahrheitswert
+>>>>>>> bc55c6027ff18d76a50606db6bb62f62cb2efcdd
      */
     public boolean istStartspieler()
     {
 
     }
+<<<<<<< HEAD
 
+=======
+    
+>>>>>>> bc55c6027ff18d76a50606db6bb62f62cb2efcdd
     /**
+<<<<<<< HEAD
      * Setzt den Stein.
      * @return Stein
+=======
+     * Diese Methode nimmt den aktuellen Stein aus dem Array (=> null setzen) und gibt
+     * das Objekt an dieser Position zurück. Der Wert aktuellerStein wird um 1 verringert.
+     * 
+     * @return Stein 
+>>>>>>> bc55c6027ff18d76a50606db6bb62f62cb2efcdd
      */
     public Stein setzeStein()
     {
